a18f7d13d5fa86787e64859c521fd96e35761f08
Taylor Poe <taylorpoe@gmail.com>
2016-03-05 15:45:35 -0800

    Add video & photo galleries

--- source/assets/js/functions.js
+++ source/assets/js/functions.js
@@ -1,3 +1,26 @@
 $( document ).ready(function() {
 
+  $('.photo-gallery').flickity({
+    cellAlign: 'center',
+    contain: true,
+    wrapAround: true,
+    imagesLoaded: true,
+    percentPosition: false,
+    setGallerySize: false,
+    pageDots: false,
+    freeScroll: true,
+    initialIndex: 8
+  });
+
+  $('.video-gallery').flickity({
+    cellAlign: 'center',
+    contain: true,
+    wrapAround: true,
+    imagesLoaded: true,
+    percentPosition: false,
+    pageDots: false,
+    freeScroll: true,
+    initialIndex: 2
+  });
+
 });
